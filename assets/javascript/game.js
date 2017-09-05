77ffffd8801b28b90c1d500c3cf6cdf420a19e0d
Keivon Arooji <karooji0@email.cpcc.edu>
2017-09-05 15:11:19 -0400

    updated index.html/game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+/*Rough outline. Will contain working code currently on index.html
+
+
 console.log("Hi");
 
 //Wait for user key to start
@@ -10,3 +13,21 @@ console.log("Hi");
 //If player reveals word and guesses > 0, win + 1. new word.
 
 var words = ["Pizza", "Spaghetti", "Lasagna", "Ravioli"];
+var currentWord = Math.floor(words.length * Math.random());
+var wins = 0;
+var guesses = 15;
+var lettersUsed = [];
+var element = document.getElementById("something");
+
+console.log(words[currentWord]);
+console.log(element);
+
+function playGame (){
+	//Accept input from user
+	//Pick random word
+	//Convert to underscore
+	//If letter == words[currentWord], convert back to letter
+
+
+}
+*/
