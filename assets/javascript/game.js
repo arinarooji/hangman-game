653f3544adcc7f171496dcb5661fcb590afbfa8b
Keivon Arooji <karooji0@email.cpcc.edu>
2017-09-05 12:31:22 -0400

    updated index.html/game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+console.log("Hi");
+
+//Wait for user key to start
+//Pick random word from array with corresponding image
+//Display underlined form of word
+//If user gets right letter, reveal letter on word.
+//Subtract one guess from each unique letter used
+//Store unique letters guessed
+//If player runs out of guesses, win + 0. new word.
+//If player reveals word and guesses > 0, win + 1. new word.
+
+var words = ["Pizza", "Spaghetti", "Lasagna", "Ravioli"];
