13a6d3d3b264725f8d7e1aac6f266977fc479c7f
Keivon Arooji <karooji0@email.cpcc.edu>
2018-01-01 17:28:10 -0500

    Responsiveness, UI fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,48 @@
-/*var score = 0;
+//Player score
+var wins   = 0;
 var losses = 0;
-		
-var music = new Audio('../audio/audioAsset.mp3');
+
+//Character nameBank, imageBank (corresponding indices)
+var nameBank    = [ "PACMAN", "XENOMORPH", "SUBZERO", "SAMUSARAN", "SCORPION", "ZELDA", "DONKEYKONG" ];
+
+var imageBank 	= [ "assets/images/pacman.jpg", "assets/images/xenomorph.png", "assets/images/subzero.png",
+					"assets/images/samusaran.jpg", "assets/images/scorpion.jpg", "assets/images/zelda.jpg",
+					"assets/images/donkeykong.jpg"];
+	
+//Sound effects	
+var music 	= new Audio('assets/audio/audioAsset.mp3');
+var correct = new Audio('assets/audio/winSound.mp3');
 music.play();
 
-function newGame(){
+newGame();
 
-	var wordBank = ["PACMAN", "XENOMORPH", "SUBZERO", "SAMUSARAN", "SCORPION", "ZELDA", "DONKEYKONG"];
-	var currentWord = Math.floor(wordBank.length * Math.random());
-	var currentPicture = ["../images/pacman.jpg", "../images/xenomorph.png", "../images/subzero.png", "../images/samusaran.jpg", "../images/scorpion.jpg", "../images/zelda.jpg", "../images/donkeykong.jpg"];
-	var hiddenWord = wordBank[currentWord].replace(/[A-Z]/g, '_');
+function newGame(){
+	//Declare a random index from 0 to nameBank.length
+	var currentWord = Math.floor(nameBank.length * Math.random());
+	//Declare hiddenWord to equal the random name, replacing all chars with underscores
+	var hiddenWord 	= nameBank[currentWord].replace(/[A-Z]/g, '_');
+
+	//Declare lettersUsed equal to an empty array, guesses equal to 15
 	var lettersUsed = [];
-	var guesses = 15;
-	//Reference to the headers in index.html
-	var element = document.getElementById("wordHere");
-	var guessCount = document.getElementById("guessCount");
-	var winCount = document.getElementById("winCount");
-	var lossCount = document.getElementById("lossCount");
-	var letterDisplay = document.getElementById("letters");
-	var pictureDisplay = document.getElementById("pictureHere");
+	var guesses 	= 15;
+
+	//Reference the specified elements in index.html
+	var winCount       = document.getElementById("winCount");
+	var lossCount  	   = document.getElementById("lossCount");
+	var element        = document.getElementById("wordHere");
+	var guessCount     = document.getElementById("guessCount");
+	var letterDisplay  = document.getElementById("letters");
+	var imageDisplay   = document.getElementById("character");
+
 	//Assign variables to the headers
-	element.innerHTML = hiddenWord;
+	element.innerHTML 	 = hiddenWord;
 	guessCount.innerHTML = guesses;
-	winCount.innerHTML = "Wins: " + score;
-	lossCount.innerHTML = "Losses: " + losses;
+	winCount.innerHTML 	 = "Wins: " + wins;
+	lossCount.innerHTML  = "Losses: " + losses;
 	letterDisplay.innerHTML = lettersUsed;
-	pictureDisplay.src = currentPicture[currentWord];
+	imageDisplay.src = imageBank[currentWord];
 			
-	var theWord = wordBank[currentWord]; //Simple reference to the word
+	var theWord    = nameBank[currentWord]; //Simple reference to the word
 	var revealWord = hiddenWord; //Will be split into array, changed, then joined
 
 
@@ -36,15 +51,16 @@ function newGame(){
 		var letter = event.key;
 		var capitalLetter = letter.toUpperCase();
 
-		//If letter not used before
-		if (!lettersUsed.includes(capitalLetter) && guesses > 0){
-			//Push to this array
+		//If letter not used before and user can guess
+		if (lettersUsed.indexOf(capitalLetter) == -1 && guesses > 0){
+			//Push to lettersUsed[] array and update the html
 			lettersUsed.push(capitalLetter);
 			letterDisplay.innerHTML = lettersUsed; //Update the header/innerHTML
 					
-			//If letter is in theWord, Note: Use .indexOf()
-			if (theWord.includes(capitalLetter)){
-				//Splits string to array, is now a mutable string
+			//If letter is in theWord
+			if (theWord.indexOf(capitalLetter) !== -1){
+				
+				//Split string to array
 				revealWord = hiddenWord.split('');
 				//Iterate through theWord. Find the index of all occurrences.
 				for (var i = 0; i < theWord.length; i++){
@@ -52,18 +68,20 @@ function newGame(){
 					if (theWord[i] === capitalLetter){
 						//Replace revealWord's underscore at that index w/ capitalLetter
 						revealWord[i] = capitalLetter;
-						//Becomes string form of that array/mutable string
+						//Convert to revealWord to string, hiddenWord equals this string
 						hiddenWord = revealWord.join('');
 						//Update page with new string
 						element.innerHTML = hiddenWord; 
 					}
 				}
+
 				//If hiddenWord matches theWord
 				if (hiddenWord === theWord){
-					score ++;
-					winCount.innerHTML = "Wins: " + score;
-					alert(hiddenWord + "!" + " Nice!");
-					newGame();
+					correct.play();
+					//Increment wins, update html
+					winCount.innerHTML = "Wins: " + (wins++);
+					//Start new game after 2 seconds
+					setTimeout(function(){ newGame(); }, 2000);
 				}
 			}
 			//If it is not in array, and not in theWord
@@ -71,14 +89,12 @@ function newGame(){
 				guesses -= 1;
 				guessCount.innerHTML = guesses;
 				if (guesses < 1){
-					losses ++;
-					lossCount.innerHTML = "Losses: " + losses;
-					alert("Don't get hung up on it, you'll do better next time!");
-					newGame();
+					//Increment losses, update html
+					lossCount.innerHTML = "Losses: " + (losses++);
+					//Start new game after 2 seconds
+					setTimeout(function(){ newGame(); }, 2000);
 				}
 			}
 		}
 	}
 }
-newGame();
-*/
